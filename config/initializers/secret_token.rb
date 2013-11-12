ed150064de9d7d274bbd142288187c2cd953f84b
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-11-11 21:25:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aretheyintome::Application.config.secret_key_base = '3b2f78cf849b0ac2c828951d6181ae8ad9f8f327edb096b90e328780d741d8049e39a2609d99e04753e8c0c219ac81510430c02834ff11198d4c46833d8374c1'
