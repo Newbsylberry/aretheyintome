d938a62166ec1d64110b8ce7c9730997a789b739
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-11-12 16:22:38 -0500

    Fixed integer bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112025545) do
+ActiveRecord::Schema.define(version: 20131112210903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "options", force: true do |t|
     t.string   "content"
-    t.decimal  "value"
+    t.decimal  "value",       precision: 10, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "question_id"
